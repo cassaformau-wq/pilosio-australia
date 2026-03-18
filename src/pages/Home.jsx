72697dd35ca17a870c0009a8a239f2807456eccb
Team Cassaform <cassaform.au@gmail.com>
2026-03-18 00:41:55 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ const heroTiles = [
     title: "Discover the Flydeck System",
     subtitle: "Innovative suspended platform solution",
     img: "https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=800&q=80",
+    page: "ProductFlydeck",
   },
   {
     title: "IXI Truss: Innovative Aluminium Beam",
